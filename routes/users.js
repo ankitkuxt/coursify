1458396538914032227ebcb86afda1f14cf93e2e
Ankit Kumawat <ankitkumawat1508@gmail.com>
2024-10-09 05:37:32 +0530

    Added Router for backend

---
+++ routes/users.js
@@ -0,0 +1,27 @@
+const { Router } = require("express");
+
+
+const userRouter = Router();
+
+    userRouter.post("/user/signup", function(req, res) {
+        res.json({
+            message: "signup endpoint"
+        })
+    })
+    
+    userRouter.post("/user/signin", function(req, res){
+        res.json({
+            message: "signin endpoint"
+        })
+    })
+
+    userRouter.get("/user/purchases", function(req, res) {
+        res.json({
+            message: "purchase endpoint"
+        })
+    })
+
+
+module.exports = {
+    userRouter: userRouter
+}
