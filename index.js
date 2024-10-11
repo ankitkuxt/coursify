0771bacafb3181da951bf424ae6ebc02d615bacb
Ankit Kumawat <ankitkumawat1508@gmail.com>
2024-10-11 12:31:48 +0530

    added the code where it only starts if connected to db

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const { userRouter } = require("./routes/users"); 
 const { courseRouter } = require("./routes/course");
@@ -9,4 +10,10 @@ app.use("/api/v1/user", userRouter);
 app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/course", courseRouter);
 
-app.listen(3000);
+async function main(){
+    await mongoose.connect("mongodb+srv://ankitkumawat1508:ankit123@cluster0.hoabjrh.mongodb.net/coursify");
+    app.listen(3000);
+    console.log("listening to port 3000");
+}
+
+main();
